f651202d2ea2587632317403fd8647581476998e
jordan-furr <jordanfurr98@gmail.com>
2024-12-16 20:41:05 +0100

    home

---
+++ component/susannah-header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header-cont ph4 borderbox align-center">
+        <div class="flex-row space-between block align-center">
+            <div class="logo">
+                <a href="/" class="menu-item">SHF</a>
+            </div>
+            <div class="block flex-row ">
+                <div class="block borderbox">
+                    <div class="header-nav-list flex-row">
+                        <div class="block pr3">
+                            <a href="/publications" class="menu-item" exact-active-class="active">uncertainty</a>
+                        </div>
+                        <div class="block pr3">
+                            <a href="/about" class="menu-item" exact-active-class="active">interiors</a>
+                        </div>
+                        <div class="block pr3">
+                            <a href="/booking" class="menu-item" exact-active-class="active">pronk</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <a href="/">in</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="home">
+        <div class="landing">
+            <h3>SHF</h3>
+            <h2>...</h2>
+        </div>
+    </div>
+</template>
